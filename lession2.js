a31bb7e70abddc3820f905715aa032d641c1e1ef
sohamrathodeww <soham.rathod@excellentwebworld.in>
2026-09-11 16:34:15 +0530

    lession 2

---
+++ lession2.js
@@ -0,0 +1,9 @@
+let username = 'soham-007';
+let age = 25;
+let salary = 50000;
+let active = true;
+
+console.log(username);
+console.log(age);
+console.log(salary);
+console.log(active);
---
+++ lession2.ts
@@ -0,0 +1,19 @@
+let username:string = 'Soham 007';
+let age:number = 20;
+let salary:number = 0;
+let active:boolean = true;
+
+console.log(username);
+console.log(age);
+console.log(salary);
+console.log(active);
+
+let std:string = '12th';
+let mainSubject:string = 'ARTS';
+
+console.log(`=========================`);
+console.log(std);
+console.log(mainSubject);
+
+
+
